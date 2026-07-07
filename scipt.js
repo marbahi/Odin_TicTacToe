713712288edb95879bd3c4d02b22bf3e287431ea
marbahi <ammar.skyagt@google.com>
2026-07-07 13:09:05 +0000

    Play From Console

---
+++ scipt.js
@@ -0,0 +1,130 @@
+const player1 = { name: "jhon doe", mark: "X"};
+const player2 = { name: "jane doe", mark: "O"};
+
+const board = {
+    layer1: ["","",""],
+    layer2: ["","",""],
+    layer3: ["","",""]
+}
+
+let turns = 0;
+
+function GamesBoard(){
+    console.log(player1)
+    console.log(player2)
+    console.log(board)
+    console.log("Inital Turns: " + turns)
+}
+
+function Turn(marked){
+    if (turns < 9) {
+        if (turns%2 === 0){
+            console.log("Player 1's Marked at " + marked);
+            if (marked < 3){
+                if (board.layer1[marked] === "") {
+                    board.layer1[marked] = player1.mark;
+                    turns ++;
+                } else {
+                    console.log("Choose another spot!");
+                }
+            } else if (marked > 2 && marked < 6) {
+                if (board.layer2[marked - 3] === "") {
+                    board.layer2[marked - 3] = player1.mark;
+                    turns ++;
+                } else {
+                    console.log("Choose another spot!");
+                }
+            } else if (marked > 5 && marked < 9) {
+                if (board.layer3[marked - 6] === "") {
+                    board.layer3[marked - 6] = player1.mark;
+                    turns ++;
+                } else {
+                    console.log("Choose another spot!");
+                }
+            }
+        } else {
+            console.log("Player 2's Marked at " + marked);
+            if (marked < 3){
+                if (board.layer1[marked] === "") {
+                    board.layer1[marked] = player2.mark;
+                    turns ++;
+                } else {
+                    console.log("Choose another spot!");
+                }
+            } else if (marked > 2 && marked < 6) {
+                if (board.layer2[marked - 3] === "") {
+                    board.layer2[marked - 3] = player2.mark;
+                    turns ++;
+                } else {
+                    console.log("Choose another spot!");
+                }
+            } else if (marked > 5 && marked < 9) {
+                if (board.layer3[marked - 6] === "") {
+                    board.layer3[marked - 6] = player2.mark;
+                    turns ++;
+                } else {
+                    console.log("Choose another spot!");
+                }
+            }
+        }
+
+        console.log(board);
+
+        if ((board.layer1[0] === "X" && board.layer1[1] === "X" && board.layer1[2] === "X") ||
+            (board.layer2[0] === "X" && board.layer2[1] === "X" && board.layer2[2] === "X") ||
+            (board.layer3[0] === "X" && board.layer3[1] === "X" && board.layer3[2] === "X") || 
+            (board.layer1[0] === "X" && board.layer2[0] === "X" && board.layer3[0] === "X") ||
+            (board.layer1[1] === "X" && board.layer2[1] === "X" && board.layer3[1] === "X") ||
+            (board.layer1[2] === "X" && board.layer2[2] === "X" && board.layer3[2] === "X") ||
+            (board.layer1[0] === "X" && board.layer2[1] === "X" && board.layer3[2] === "X") ||
+            (board.layer1[2] === "X" && board.layer2[1] === "X" && board.layer3[0] === "X")){
+            console.log("Player 1 Win!!");
+            
+            turns = 0;
+            for(let i = 0; i < 3; i++){
+                board.layer1[i] = "";
+                board.layer2[i] = "";
+                board.layer3[i] = "";
+            }
+        } else if ((board.layer1[0] === "X" && board.layer1[1] === "X" && board.layer1[2] === "X") ||
+            (board.layer2[0] === "O" && board.layer2[1] === "O" && board.layer2[2] === "O") ||
+            (board.layer3[0] === "O" && board.layer3[1] === "O" && board.layer3[2] === "O") || 
+            (board.layer1[0] === "O" && board.layer2[0] === "O" && board.layer3[0] === "O") ||
+            (board.layer1[1] === "O" && board.layer2[1] === "O" && board.layer3[1] === "O") ||
+            (board.layer1[2] === "O" && board.layer2[2] === "O" && board.layer3[2] === "O") ||
+            (board.layer1[0] === "O" && board.layer2[1] === "O" && board.layer3[2] === "O") ||
+            (board.layer1[2] === "O" && board.layer2[1] === "O" && board.layer3[0] === "O")){
+            console.log("Player 2 Win!!");
+            
+            turns = 0;
+            for(let i = 0; i < 3; i++){
+                board.layer1[i] = "";
+                board.layer2[i] = "";
+                board.layer3[i] = "";
+            }
+        }
+    } else {
+        console.log("Game Draw");
+        
+        turns = 0;
+        for(let i = 0; i < 3; i++){
+            board.layer1[i] = "";
+            board.layer2[i] = "";
+            board.layer3[i] = "";
+        }
+    }
+}
+// Untuk game
+// Membuat player
+// Selanjutnya membuat sebuah papan
+// lalu player pertama input data ke dalam papan
+// maka huruf X / O akan muncul
+// Setelah itu ganti giliran
+// player kedua input data ke dalam papan
+// maka huruf X / O akan muncul
+// dilakukan sampai ada orang yang memiliki 3 O / X secara diagonal, horizontal, dan Vertikal
+// jika ada, maka player tersebut menang, dan player lain kalah
+// jika tidak ada, maka draw
+// ulang permainan
+
+const play = GamesBoard();
